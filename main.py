789fd16195db6a2d9086b259f08f5310736493ee
Tkachev Valery <ebalaika1994@gmail.com>
2022-01-02 17:29:53 +0300

    added sent on time

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ import requests
 import telebot
 from geopy import geocoders
 from translate import Translator
+from multiprocessing.context import Process
+import schedule
+import time
+
+
+def start_process():
+    p1 = Process(target=P_schedule.start_schedule, args=()).start()
+
+
+class P_schedule():
+    def start_schedule():
+        schedule.every().day.at("10:00").do(resent_message)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
 
 def translate_weather(words):
@@ -31,6 +47,15 @@ def weather_in_city(latitude, longitude):
     return dict_weather
 
 
+def resent_message():
+    for client in clients:
+        city = clients[client]
+        latitude, longitude = geo_pos(city)
+        you_weather = weather_in_city(latitude, longitude)
+        bot.send_message(client, f' Температура сейчас {you_weather["temp"]}!'
+                                 f' А на небе {you_weather["weather"]}.')
+
+
 def print_weather(dict_weather, message):
     bot.send_message(message.from_user.id, f' Температура сейчас {dict_weather["temp"]}!'
                                            f' А на небе {dict_weather["weather"]}.')
@@ -38,18 +63,28 @@ def print_weather(dict_weather, message):
 
 bot = telebot.TeleBot("5027574826:AAFag0KMdDDpvg6WJa9j5JSgEczmbqsYtQE")
 
+clients = {336065295: "Москва"}
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_massage(message):
-    bot.send_message(message.from_user.id, f'Я Sebastian - бот погоды, приятно познакомитсья, {message.from_user.first_name},'
-                          f' Напишите название города, в котором хотите узнать погоду.')
+    bot.send_message(message.from_user.id,
+                     f'Я Sebastian - бот погоды, приятно познакомитсья, {message.from_user.first_name},'
+                     f' Напишите название города, в котором хотите узнать погоду.')
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     city = message.text
+    clients[message.from_user.id] = city
     latitude, longitude = geo_pos(city)
     you_weather = weather_in_city(latitude, longitude)
     print_weather(you_weather, message)
 
 
-bot.infinity_polling()
+if __name__ == '__main__':
+    start_process()
+    try:
+        bot.infinity_polling()
+    except:
+        pass
